4c49cfc8816d223852bc875f08f46cfadfc4e2cb
sxqsachin <sxqsachin@yahoo.com>
2021-11-10 17:53:36 +0800

    up: 交易面板

--- src/components/ui/trade-panel.tsx
+++ src/components/ui/trade-panel.tsx
@@ -19,30 +19,44 @@ function ItemTradePanel({ role, shopper, shopkeeper, priceList, onItemClick }: I
   return (
     <>
       <div>{shopkeeper.data.name}</div>
-      <hr className="my-4" />
-      {Object.keys(shopkeeper.items).map((itemID) => {
-        const salePrice = role === 'shopper' ? priceList?.[itemID]?.enterPrice: priceList?.[itemID]?.salePrice;
+      <hr className="my-4 border-gray-300" />
+      <div>
+        <div className="mb-2 pb-2 border-b border-gray-200">
+          <span className="inline-block w-3/6 text-left">物品名称</span>
+          <span className="inline-block w-2/6 text-left">{role==='shopper' ? '出售价' : '购买价'}</span>
+          <span className="inline-block w-1/6 text-right">数量</span>
+        </div>
+        {Object.keys(shopkeeper.items).map((itemID) => {
+          const salePrice = role === 'shopper' ? priceList?.[itemID]?.enterPrice: priceList?.[itemID]?.salePrice;
 
-        let canAfford = true;
-        if (salePrice) {
-          if (!shopper.items[salePrice.subject] || shopper.items[salePrice.subject].length < salePrice.amount) {
-            canAfford = false;
+          let canAfford = true;
+          if (salePrice) {
+            if (!shopper.items[salePrice.subject] || shopper.items[salePrice.subject].length < salePrice.amount) {
+              canAfford = false;
+            }
           }
-        }
 
-        const onClick = canAfford ? () => onItemClick(itemID, salePrice ?? ({} as any as ItemPrice)) : noop;
+          let hasStore = !!shopkeeper.items[itemID].length;
 
-        return (
-          <div
-            className={"cursor-pointer" + (canAfford ? '' : ' text-gray-300 cursor-not-allowed') }
-            key={itemID}
-            onClick={onClick}
-          >
-            <span>{Items.get(itemID).data.name}</span>
-            <span className="float-right">{shopkeeper.items[itemID].length}</span>
-          </div>
-        );
-      })}
+          if (!hasStore) {
+            return <></>;
+          }
+
+          const onClick = (hasStore && canAfford) ? () => onItemClick(itemID, salePrice ?? ({} as any as ItemPrice)) : noop;
+
+          return (
+            <div
+              className={"cursor-pointer w-full my-2" + ((canAfford && hasStore) ? '' : ' text-gray-300 cursor-not-allowed') }
+              key={itemID}
+              onClick={onClick}
+            >
+              <span className="inline-block w-3/6 text-left">{Items.get(itemID).data.name}</span>
+              <span className="inline-block w-2/6 text-left">{salePrice ? `${salePrice.amount}${Items.get(salePrice.subject).data.name}` : '免费'}</span>
+              <span className="inline-block w-1/6 text-right">{shopkeeper.items[itemID].length}</span>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
