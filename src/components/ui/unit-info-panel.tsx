c5383d706db0fcf129d095a7d2067ab816142e85
sxqsachin <sxqsachin@yahoo.com>
2021-11-15 16:05:05 +0800

    up: game icon

--- src/components/ui/unit-info-panel.tsx
+++ src/components/ui/unit-info-panel.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Component, useState } from "react";
 import { ItemAction } from "@/types/action";
 import { IItem } from "@/types/Item";
 import { IUnit } from "@/types/Unit";
@@ -6,12 +6,22 @@ import { UnitEquipmentPanel } from "./unit-equipment-panel";
 import { UnitItemPanel } from "./unit-item-panel";
 import { UnitStatusPanel } from "./unit-status-panel";
 import { UnitStatePanel } from "./unit-state-panel";
+import { GiSkills, GiBackpack, GiBattleGear, GiHistogram } from 'react-icons/gi';
+import { IconType } from "react-icons/lib";
 
 type UnitInfoPanelParam = {
   unit: IUnit,
   onItemAction: (action: ItemAction, item: IItem) => void;
 }
 type PanelType = 'STATUS' | 'STATE' | 'ITEM' | 'EQUIPMENT';
+
+const PanelList: {type: PanelType, name: string, icon: IconType}[] = [
+  { type: 'STATUS', name: '属性', icon: GiHistogram },
+  { type: 'STATE', name: '状态', icon: GiSkills },
+  { type: 'ITEM', name: '物品', icon: GiBackpack },
+  { type: 'EQUIPMENT', name: '装备', icon: GiBattleGear },
+];
+
 export function UnitInfoPanel({unit, onItemAction}: UnitInfoPanelParam) {
   const [curPanel, setCurPanel] = useState<PanelType>('STATUS');
 
@@ -28,10 +38,16 @@ export function UnitInfoPanel({unit, onItemAction}: UnitInfoPanelParam) {
         { curPanel === 'EQUIPMENT' && <UnitEquipmentPanel unit={unit} onItemAction={onItemAction}></UnitEquipmentPanel> }
       </div>
       <div className="absolute bottom-0 w-full flex flex-row justify-between border-t p-4 pb-0 h-18">
-        <button onClick={() => onPanelChange('STATUS')}>Status</button>
-        <button onClick={() => onPanelChange('STATE')}>Status</button>
-        <button onClick={() => onPanelChange('ITEM')}>Item</button>
-        <button onClick={() => onPanelChange('EQUIPMENT')}>Equipment</button>
+        {
+          PanelList.map(panelInfo => {
+            const { type, name } = panelInfo;
+            return (
+              <button onClick={() => onPanelChange(type)} title={name}>
+                <panelInfo.icon className={"text-2xl " + (curPanel === type ? "text-blue-600" : "text-gray-600")}></panelInfo.icon>
+              </button>
+            );
+          })
+        }
       </div>
     </div>
   )
